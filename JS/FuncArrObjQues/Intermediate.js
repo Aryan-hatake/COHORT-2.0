04fda1f7a8c9054211a5d934750172d160bd9922
Aryan <aryangadhave202@gmail.com>
2025-11-12 13:55:54 +0530

    funcarrobj ques done

---
+++ JS/FuncArrObjQues/Intermediate.js
@@ -0,0 +1,96 @@
+// function runTwice(func){
+//     func();
+//     func();
+// }
+
+// runTwice(function(){ console.log("inner func") }) //or runTwice(()=>{console.log("inner func")})
+
+//--------------------------------------------------------------
+
+// function pure(params){
+//    return "anything"
+// }
+// pure("args")
+
+// let a = 0;
+// function impure(param){
+//     a+=param
+// }
+// impure(2)
+
+
+//---------------------------------------------------------------
+
+// let obj = {
+//     name:"rashi",
+//     age:18
+// }
+
+// function objDestructuring({name,age},obj){
+//     console.log()
+//     const {name:n2,age:a2} = obj
+//     console.log(n2,a2)
+//     console.log(name,age)
+// }
+// objDestructuring(obj,obj)
+
+//-----------------------------------------------------------------
+
+// let arr = [1,2,3,4,5];
+
+// let sqre = arr.map((e)=>{
+//     return e*e
+// })
+// console.log(sqre)
+
+//------------------------------------------------------------------
+
+// let arr = [1,2,3,4,5,6];
+
+// let even = arr.filter((e)=>{
+//     return e%2==0
+// })
+
+// console.log(even)
+
+//--------------------------------------------------------------------
+
+// let arr = [1000,2000,3000];
+
+// let totalSalary = arr.reduce((acc,b)=>{
+//             return acc+b
+// },0)
+// console.log(totalSalary)
+
+//----------------------------------------------------------------------
+
+// let names = ["aryan","rashi","dev","surekha","vijay"]
+
+// let ans = names.every((e)=>{
+//     return e.length>3
+// })
+// console.log(ans)
+
+//-----------------------------------------------------------------------
+
+// let obj = {
+//     name:"aryan",
+//     age:19
+// }
+// // Object.freeze(obj) prevents modications as well addition and deletion of keys
+
+// // Object.seal(obj)  allows modification but not addition and deletion of keys
+
+// console.log(obj)
+
+//---------------------------------------------------------------------------
+
+// let obj ={
+//     user:{
+//         address:{
+//             city:"Om residency"
+//         }
+//     }
+// }
+
+// console.log(obj?.user?.address?.city)
---
+++ JS/FuncArrObjQues/script.js
@@ -0,0 +1,86 @@
+// function sayHello(){
+//     console.log(`Hello Javascript`)
+// }
+// sayHello()
+
+// -----------------------------------------------------------
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(2,3))
+
+//-------------------------------------------------------------
+
+// function helloGuest(name="Guest"){
+//     console.log(`Hi ${name}`)
+// }
+// helloGuest("aryan")
+
+//--------------------------------------------------------------
+
+// function addInfinite(...vals){
+//     let sum =0;
+//     vals.forEach((e)=>{
+//         sum+=e
+//     })
+    
+//     return sum;
+// }
+
+// console.log(addInfinite(1,2,3))
+
+//---------------------------------------------------------------
+
+// (function (){
+//     console.log("I run Instantly")
+// })()
+
+//---------------------------------------------------------------
+
+// function parent(){
+//     let a = 22;
+
+//     function child(value){
+//         console.log(value)
+//     }
+
+//     child(a)
+// }
+
+// parent()
+
+//----------------------------------------------------------------
+
+// let arr = ["apple","kiwi","tangerine","banana","tomato"]
+// arr.push("extra fruit")
+// arr.shift()
+// console.log(arr)
+
+//-----------------------------------------------------------------
+
+// let arr = [1,2,3,4,5];
+
+// for(let i in arr){
+//     console.log(arr[i])
+// }
+
+//------------------------------------------------------------------
+
+// let person = {
+//     name:"rashi",
+//     age:18,
+//     city:"Om residency"
+// }
+
+// for(let key in person){
+//     console.log(person[key])
+// }
+
+//--------------------------------------------------------------------
+
+// setTimeout(() => {
+//     console.log("Times up!")
+// }, 2000);
+
+//--------------------------------------------------------------------
